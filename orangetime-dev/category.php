c6ee6a1c64618be148e238995e40645734683291
Saborknight <anton@brink.es>
2017-01-04 02:48:27 +0200

    Refactor - Work Content Overlay Change Part 2
    What
    =================
    Changed overlays. Hover effects added. Only need to add highlighted now.
    
    Ticket: N/A

--- orangetime-dev/category.php
+++ orangetime-dev/category.php
@@ -39,32 +39,67 @@
             {
                 the_post();
 
-                // Kui tegemist on koverentsi kategooriaga ja viimane postitus on kuvatud,
-                // muuda selle linki ja viita see Portfelli lehele
-                if( 4 == icl_object_id($current_cat_object->cat_ID, 'category') && $found_posts-- == 1 ) {
-                    $last = true;
+                // Specify Templates
+                if( 4 == icl_object_id($current_cat_object->cat_ID, 'category') ) { // Conference
+                    $post_index = $wp_query->current_post + 1;
+                    $highlighted = false;
+                    var_dump($post_index);
+
+                    // Kui tegemist on koverentsi kategooriaga ja viimane postitus on kuvatud,
+                    // muuda selle linki ja viita see Portfelli lehele
+                    if( $found_posts-- == 1 ) {
+                        $last = true;
+                    }
+
+                    // if( $post_index =< 2 ) {
+                    //     $highlighted = true;
+                    // }
+
+                    $cat_template = 
+                        oi_template(
+                            '<li class="col-xs-6 col-sm-4 col-md-%(width)s work-item">
+                                <article class="work">
+                                    <a href="%(url)s" class="work-url">
+                                        <figure class="work-image">%(featured_image)s</figure>
+                                        <div class="work-content">
+                                            <div class="work-title-container">
+                                                <h2 class="work-title"><span>%(title)s</span></h2>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </article>
+                            </li>',
+
+                            array(
+                                'width' => ( $highlighted ) ? '6' : '3',
+                                'featured_image' => get_the_post_thumbnail(get_the_ID(), 'category-image'),
+                                'title' => get_the_title(),
+                                'url' => ( $last ) ? get_category_link(icl_object_id(5, 'category')) : get_permalink()
+                            )
+                        );
+                } else { // Default (Used for "Events" == 5)
+                    $cat_template =
+                        oi_template(
+                            '<li class="col-xs-6 col-sm-4 col-md-3">
+                                <article class="work">
+                                    <a href="%(url)s" class="work-url">
+                                        <figure class="work-image">%(featured_image)s</figure>
+                                        <div class="work-content">
+                                            <h2 class="work-title"><span>%(title)s</span></h2>
+                                        </div>
+                                    </a>
+                                </article>
+                            </li>',
+
+                            array(
+                                'featured_image' => get_the_post_thumbnail(get_the_ID(), 'category-image'),
+                                'title' => get_the_title(),
+                                'url' => ( $last ) ? get_category_link(icl_object_id(5, 'category')) : get_permalink()
+                            )
+                        );
                 }
 
-                array_push($output,
-                    oi_template(
-                        '<li class="col-xs-6 col-sm-4 col-md-3">
-                            <article class="work">
-                                <a href="%(url)s" class="work-url">
-                                    <figure class="work-image">%(featured_image)s</figure>
-                                    <div class="work-content">
-                                        <h2 class="work-title"><span>%(title)s</span></h2>
-                                    </div>
-                                </a>
-                            </article>
-                        </li>',
-
-                        array(
-                            'featured_image' => get_the_post_thumbnail(get_the_ID(), 'category-image'),
-                            'title' => get_the_title(),
-                            'url' => ( $last ) ? get_category_link(icl_object_id(5, 'category')) : get_permalink()
-                        )
-                    )
-                );
+                array_push($output, $cat_template);
             }
         }
         ?>
